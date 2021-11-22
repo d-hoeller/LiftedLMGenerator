3483fd87704bc23cacefa13152bc16455d11a2ac
Daniel Höller <hoeller@cs.uni-saarland.de>
2021-11-22 15:29:41 +0100

    added classes for LM graphs

---
+++ src/lm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dh on 22.11.21.
+//
+
+#include "lm.h"
---
+++ src/lmGraph.h
@@ -0,0 +1,14 @@
+//
+// Created by dh on 22.11.21.
+//
+
+#ifndef SRC_LMGRAPH_H
+#define SRC_LMGRAPH_H
+
+
+class lmGraph {
+
+};
+
+
+#endif //SRC_LMGRAPH_H
